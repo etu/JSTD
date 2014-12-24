8af137e8c9770f8d33373c8b20b276c21f49d56c
Elis Axelsson <elis.axelsson@gmail.com>
2014-12-24 11:15:34 +0100

    Added base JS files

---
+++ js/environment.js
@@ -0,0 +1,115 @@
+
+/**
+ * Environment Class
+ */
+var Environment = new Class({
+    Implements: [ Options ],
+    options: {
+        gameObjects: [],
+        width: 640,
+        height: 480,
+        pause: false
+    },
+    initialize: function(options) {
+        this.setOptions(options);
+
+        this.options.ctxs = {
+            background: $('background').getContext('2d'),
+            screen: $('screen').getContext('2d')
+        }
+
+        return this;
+    }
+});
+
+var Enviroment_old = new Class({
+    Implements: [ Options ],
+    options: {
+	gameObjects: [],
+	width:  640,
+	height: 480,
+	pause:  false
+    },
+    initialize: function(options) {
+	env = this; // Set this to the global env variable
+
+	this.setOptions(options);
+
+	this.options.ctxs = {
+	    background: $('background').getContext('2d'),
+	    screen:     $('screen').getContext('2d')
+	}
+
+	this.options.starfield = new StarField();
+	this.options.player    = new Player();
+    },
+    gameLoop: function() {
+	var self = this;
+
+	/**
+	 * Move ALL the things!
+	 */
+	self.options.player.move();
+
+	Array.each(self.options.gameObjects, function(object) {
+	    object.move();
+	});
+
+	/**
+	 * Draw ALL the things!
+	 */
+	self.options.ctxs.screen.clearRect(0, 0, self.options.width, self.options.height);
+
+	self.options.player.draw();
+
+	Array.each(self.options.gameObjects, function(object) {
+	    object.draw();
+	});
+
+	/**
+	 * Move/Draw starfield, it got it's own canvas and own life anyways
+	 */
+	self.options.starfield.move();
+
+	/**
+	 * If not paused, request a new frame
+	 */
+	if(!self.options.pause) {
+	    window.requestAnimationFrame((function() {
+		self.gameLoop();
+	    }));
+	}
+    },
+    unpause: function() {
+	var self = this,
+	    time = Date.now();
+
+	/**
+	 * Hide the pausescreen
+	 */
+	$('pausescreen').setStyle('display', 'none');
+
+	/**
+	 * Update all timingevents
+	 */
+	this.options.player.options.lastUpdateTime    = time;
+	this.options.starfield.options.lastUpdateTime = time;
+
+	/**
+	 * unpause
+	 */
+	this.options.pause = false;
+
+	/**
+	 * Request a new timed frame
+	 */
+	window.requestAnimationFrame((function() {
+	    self.gameLoop();
+	}));
+    },
+    pause: function() {
+	this.options.pause = true;
+
+	$('pausescreen').setStyle('display', 'block');
+    }
+});
---
+++ js/game.js
@@ -0,0 +1,79 @@
+
+'use strict';
+
+console.log('loaded');
+
+var env;
+//var env, keyStates = {};
+
+
+/**
+ * Launch game on load of all resources
+ */
+window.addEvent('load', function() {
+    env = new Environment();
+});
+/*
+window.addEvent('load', function() {
+    new Enviroment();
+
+    window.requestAnimationFrame((function () {
+	env.gameLoop();
+    }));
+
+    // unpause button :)
+    $('unpause').addEvent('click', function(e) {
+	env.unpause();
+    });
+});
+*/
+
+// Automagic pause if you loose focus of the game
+/*
+window.addEvent('blur', function() {
+    env.pause();
+});
+*/
+
+
+// Record keypresses
+/*
+window.addEvent('keydown', function(e) {
+    if(keyStates[e.key] !== null) keyStates[e.key] = true;
+
+    if(keyStates.p) { // Hotkey for pausing/unpausing
+	if(env.options.pause) env.unpause();
+	else env.pause();
+    }
+});
+window.addEvent('keyup',   function(e) { delete keyStates[e.key]; });
+*/
+
+
+/**
+ * BaseClass with basic creating of objects and basic structure
+ */
+/*
+var BaseClass = new Class({
+    Implements: [ Options ],
+    options: {
+	lastUpdateTime: Date.now(),
+	x: 0,
+	y: 0
+    },
+    initialize: function(options) {
+	// Override some default options, if I put the random operations
+	// in the options object, it will be the same random for all objects :-)
+	this.options.x = Number.random(0, env.width);
+	this.options.y = Number.random(0, env.height);
+
+	this.setOptions(options);
+
+	env.gameObjects.push(this);
+    },
+    move: function() {
+    },
+    draw: function() {
+    }
+});
+*/
