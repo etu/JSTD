a67534cb0ebd23114864fe5f88067def582716a6
Elis Axelsson <elis.axelsson@gmail.com>
2014-12-24 14:33:03 +0100

    Moved code

--- js/game.js
+++ js/game.js
@@ -27,22 +27,25 @@ window.addEvent('blur', function() {
     env.pause();
 });
 
-
-
-
-
 // Record keypresses
-/*
 window.addEvent('keydown', function(e) {
-    if(keyStates[e.key] !== null) keyStates[e.key] = true;
+    if (keyStates[e.key] !== null) {
+        keyStates[e.key] = true;
+    }
 
-    if(keyStates.p) { // Hotkey for pausing/unpausing
-	if(env.options.pause) env.unpause();
-	else env.pause();
+    if (keyStates.p) { // Hotkey for pausing/unpausing
+        if (env.options.pause) {
+            env.unpause();
+        } else {
+            env.pause();
+        }
     }
 });
-window.addEvent('keyup',   function(e) { delete keyStates[e.key]; });
-*/
+window.addEvent('keyup', function(e) {
+    delete keyStates[e.key];
+});
+
+
 
 
 /**
