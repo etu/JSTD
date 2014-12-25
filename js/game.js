8d2bd400ca5d61d400fd4975c3fbe895138b621a
Elis Axelsson <elis.axelsson@gmail.com>
2014-12-25 21:04:13 +0100

    Move code around

--- js/game.js
+++ js/game.js
@@ -1,7 +1,4 @@
-
 'use strict';
-
-console.log('loaded');
 
 var env, keyStates = {};
 
@@ -16,32 +13,42 @@ window.addEvent('load', function() {
         env.gameLoop();
     }));
 
+
+    /**
+     * Add support for unpause button
+     */
     $('unpause').addEvent('click', function() {
         env.unpause();
     });
-});
 
-// Automagic pause if you loose focus of the game
-window.addEvent('blur', function() {
-    env.pause();
-});
 
-// Record keypresses
-window.addEvent('keydown', function(e) {
-    if (keyStates[e.key] !== null) {
-        keyStates[e.key] = true;
-    }
+    /**
+     * Automagic pause if you loose focus of the game
+     */
+    window.addEvent('blur', function() {
+        env.pause();
+    });
 
-    if (keyStates.p) { // Hotkey for pausing/unpausing
-        if (env.options.pause) {
-            env.unpause();
-        } else {
-            env.pause();
+
+    /**
+     * Record keypresses
+     */
+    window.addEvent('keydown', function(e) {
+        if (keyStates[e.key] !== null) {
+            keyStates[e.key] = true;
         }
-    }
-});
-window.addEvent('keyup', function(e) {
-    delete keyStates[e.key];
+
+        if (keyStates.p) { // Hotkey for pausing/unpausing
+            if (env.options.pause) {
+                env.unpause();
+            } else {
+                env.pause();
+            }
+        }
+    });
+    window.addEvent('keyup', function(e) {
+        delete keyStates[e.key];
+    });
 });
 
 
@@ -54,19 +61,19 @@ window.addEvent('keyup', function(e) {
 var BaseClass = new Class({
     Implements: [ Options ],
     options: {
-	lastUpdateTime: Date.now(),
-	x: 0,
-	y: 0
+        lastUpdateTime: Date.now(),
+        x: 0,
+        y: 0
     },
     initialize: function(options) {
-	// Override some default options, if I put the random operations
-	// in the options object, it will be the same random for all objects :-)
-	this.options.x = Number.random(0, env.width);
-	this.options.y = Number.random(0, env.height);
+        // Override some default options, if I put the random operations
+        // in the options object, it will be the same random for all objects :-)
+        this.options.x = Number.random(0, env.width);
+        this.options.y = Number.random(0, env.height);
 
-	this.setOptions(options);
+        this.setOptions(options);
 
-	env.gameObjects.push(this);
+        env.gameObjects.push(this);
     },
     move: function() {
     },
